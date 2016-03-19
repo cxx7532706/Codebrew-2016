9c3cc0b50f108f419dc13a7b30b9e3fb6f24decc
Oscar <yuszy@outlook.com>
2016-03-19 20:28:08 +1100

    Add new post

--- Server/RailsServer/app/controllers/posts_controller.rb
+++ Server/RailsServer/app/controllers/posts_controller.rb
@@ -2,8 +2,7 @@ require 'json'
 
 class PostsController < ApplicationController
 
-  protect_from_forgery :secret => 'any_phrase',
-  :except => :collect
+  protect_from_forgery :secret => 'any_phrase', :except => :collect
 
 	def index
     #include Paperclip::Glue
@@ -18,32 +17,29 @@ class PostsController < ApplicationController
   end
 
   def collect
+    puts request.headers
+
+    @username = request.headers["Username"]
+    @password = request.headers["Password"]
+
     render text: 1
-    #puts request.headers
-
-    #@username = request.headers["Username"]
-    #@password = request.headers["Password"]
-
-    #render text: 1
-
-    #render "data received";
   end
 
   def testpost
-    @params = {'box1' => 'It is ethics. It is honor. -Emily Post', 
-      'button1' => 'Submit'}
-    @x = Net::HTTP.post_form(URI.parse('https://go-get-it.herokuapp.com/collect'), params)
-    puts @x.body
+    @params = {'Username' => 'It is ethics. It is honor. -Emily Post', 
+      'Password' => 'Submit'}
+    @x = Net::HTTP.post_form(URI.parse('https://go-get-it.herokuapp.com/collect'), @params)
+    #render text: @x.body
 
     #connection.post("https://go-get-it.herokuapp.com/collect", );
   end
 
   def new
-    
+    @post = Post.new
   end
 
   def create
-
+    @post = Post.new(params[:post])
   end
 
   def edit

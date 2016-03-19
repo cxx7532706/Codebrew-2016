dada0b9ce6eafcb3957e568196bd267b3cd8efbf
Oscar <yuszy@outlook.com>
2016-03-19 19:49:28 +1100

    Minors changes

--- Server/RailsServer/app/controllers/posts_controller.rb
+++ Server/RailsServer/app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
+require 'json'
+
 class PostsController < ApplicationController
+
+  protect_from_forgery :secret => 'any_phrase',
+  :except => :collect
 
 	def index
     #include Paperclip::Glue
@@ -10,6 +15,27 @@ class PostsController < ApplicationController
     #include Paperclip::Glue
 
     @post = Post.find(params[:id])
+  end
+
+  def collect
+    render text: 1
+    #puts request.headers
+
+    #@username = request.headers["Username"]
+    #@password = request.headers["Password"]
+
+    #render text: 1
+
+    #render "data received";
+  end
+
+  def testpost
+    @params = {'box1' => 'It is ethics. It is honor. -Emily Post', 
+      'button1' => 'Submit'}
+    @x = Net::HTTP.post_form(URI.parse('https://go-get-it.herokuapp.com/collect'), params)
+    puts @x.body
+
+    #connection.post("https://go-get-it.herokuapp.com/collect", );
   end
 
   def new

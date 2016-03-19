a1ea2fb2144fce5cfa4d0f5d862a25f99384ae7e
Oscar <yuszy@outlook.com>
2016-03-20 00:47:45 +1100

    Post new items are now working

--- Server/RailsServer/app/controllers/posts_controller.rb
+++ Server/RailsServer/app/controllers/posts_controller.rb
@@ -26,9 +26,9 @@ class PostsController < ApplicationController
   end
 
   def testpost
-    @params = {'Username' => 'It is ethics. It is honor. -Emily Post', 
-      'Password' => 'Submit'}
-    @x = Net::HTTP.post_form(URI.parse('https://go-get-it.herokuapp.com/collect'), @params)
+    #@params = {'Username' => 'It is ethics. It is honor. -Emily Post', 
+    #  'Password' => 'Submit'}
+    #@x = Net::HTTP.post_form(URI.parse('https://go-get-it.herokuapp.com/collect'), @params)
     #render text: @x.body
 
     #connection.post("https://go-get-it.herokuapp.com/collect", );
@@ -39,7 +39,21 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params[:post])
+    expir = DateTime.new(params[:post]["expir(1i)"].to_i, params[:post]["expir(2i)"].to_i, params[:post]["expir(3i)"].to_i, params[:post]["expir(4i)"].to_i, params[:post]["expir(5i)"].to_i)
+    available_time_start = DateTime.new(params[:post]["available_time_start(1i)"].to_i, params[:post]["available_time_start(2i)"].to_i, params[:post]["available_time_start(3i)"].to_i, params[:post]["available_time_start(4i)"].to_i, params[:post]["available_time_start(5i)"].to_i)
+    available_time_end = DateTime.new(params[:post]["available_time_end(1i)"].to_i, params[:post]["available_time_end(2i)"].to_i, params[:post]["available_time_end(3i)"].to_i, params[:post]["available_time_end(4i)"].to_i, params[:post]["available_time_end(5i)"].to_i)
+
+    @post = Post.new(post_params)
+
+    @post.update(expir: expir)
+    @post.update(available_time_start: available_time_start)
+    @post.update(available_time_end: available_time_end)
+
+    if @post.save
+      redirect_to posts_path#, :notice => 'Successfully uploaded'
+    else
+      render "new"
+    end
   end
 
   def edit
@@ -52,5 +66,16 @@ class PostsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def post_params
+    params.require(:post).permit(:item, :description, :quantity, :loc, :super, :org)
   end
 end
--- Server/RailsServer/config/application.rb
+++ Server/RailsServer/config/application.rb
@@ -19,7 +19,7 @@ module RailsServer
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    
     #config.api_only = true
 
     # Do not swallow errors in after_commit/after_rollback callbacks.

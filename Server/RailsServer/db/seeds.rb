5cfd4cb78fea50449d79e20f7cd143963c6f36be
Oscar <yuszy@outlook.com>
2016-03-19 15:58:23 +1100

    Updated seed file

--- Server/RailsServer/db/seeds.rb
+++ Server/RailsServer/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(:item => "Potatoes", :quantity => 10, :expir => 1, :picktime => 1, 
+  :loc => "Loc", :super => "IGA", :org => "org1")
